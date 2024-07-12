a3c51436e1612e9529b276aab23992e013bceaa7
brahim <139687519+rajelmn@users.noreply.github.com>
2024-07-12 20:58:50 +0000

    Add files via upload

---
+++ weather app/script.js
@@ -0,0 +1,42 @@
+const input = document.getElementById('input');
+const search = document.getElementById('search') 
+const degrees = document.getElementById("degrees");
+const weatherStatus = document.getElementById("status");
+const main = document.querySelector('main');
+const cloudImage = document.querySelector(".cloud");
+const clearImage = document.querySelector(".clear")
+search.addEventListener('click', () => {
+    (async function() {
+        try {
+      const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=cb642d8bcb13d37ea189cb1e1b28dd61`);
+      const response = await weatherData.json();
+      console.log(response.main.temp)
+      const weatherCalc = Math.round(response.main.temp - 273.15)
+      degrees.innerHTML = weatherCalc + "°C";
+      
+      if(weatherCalc > 18) {
+          clearImage.style.display = 'inline';
+          cloudImage.style.display = 'none'
+      } else {
+          cloudImage.style.display = 'inline';
+          clearImage.style.display = 'none'
+      }
+      
+      main.style.display = 'block';
+      }
+      catch {
+          alert(`${input.value} is not a valid city`)
+      }
+  })()
+});
+
+
+
+input.addEventListener('keydown', () => {
+    if(!input.value) {
+        main.style.display = 'none'
+    }
+})
+
+// displayWeather()
+// key = cb642d8bcb13d37ea189cb1e1b28dd61
