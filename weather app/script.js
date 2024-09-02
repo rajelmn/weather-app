e4476e088f63bbd2dec6c4a20a674b96aaedf536
brahim <rajelghmn@gmail.com>
2024-09-02 16:57:58 +0000

    removing IIFE

--- weather app/script.js
+++ weather app/script.js
@@ -6,8 +6,7 @@ const main = document.querySelector('main');
 const cloudImage = document.querySelector(".cloud");
 const clearImage = document.querySelector(".clear")
 
-search.addEventListener('click', () => {
-    (async function() {
+search.addEventListener('click', async () => {
         try {
       const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=cb642d8bcb13d37ea189cb1e1b28dd61`);
       const response = await weatherData.json();
@@ -30,11 +29,8 @@ search.addEventListener('click', () => {
       catch {
           alert(`${input.value} is not a valid city`)
       }
-  })()
+  })
     
-});
-
-
 
 input.addEventListener('keydown', () => {
     if(!input.value) {
